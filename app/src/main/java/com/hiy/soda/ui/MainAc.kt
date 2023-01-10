bc252859b24e1d1d2851546b60fdff16d588ddae
liusaideng <liusaideng@gmail.com>
2023-01-10 11:30:31 +0800

    reset

--- app/src/main/java/com/hiy/soda/ui/MainAc.kt
+++ app/src/main/java/com/hiy/soda/ui/MainAc.kt
@@ -8,16 +8,23 @@ import android.view.View
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
+import com.drakeet.multitype.MultiTypeAdapter
 import com.hiy.monbie.core.BaseBusinessAc
 import com.hiy.monbie.core.HiyHelper
 import com.hiy.monbie.core.PageState
 import com.hiy.monbie.core.PageViewModel
 import com.hiy.soda.R
+import com.hiy.soda.bean.bo.GridBo
 import com.hiy.soda.bean.dto.User
 import com.hiy.soda.database.AppDatabase
+import com.hiy.soda.helper.ItemClickSupport
+import com.hiy.soda.helper.SodaConstant
 import com.hiy.soda.helper.startup.GsonHelper
 import com.hiy.soda.provider.IProvider
+import com.hiy.soda.ui.adapter.GridItemViewBinder
 import com.hiy.soda.ui.fg.LifeCycleFg
 import com.kunminx.architecture.domain.message.MutableResult
 import kotlinx.coroutines.*
@@ -30,6 +37,10 @@ class MainAc : BaseBusinessAc<CountViewModel>() {
             AppDatabase::class.java, "database-name"
         ).build()
     }
+
+
+    private lateinit var mRv: RecyclerView
+    private val mData = mutableListOf<Any>()
 
     override fun getViewModelClass(): Class<CountViewModel> {
         return CountViewModel::class.java
@@ -71,26 +82,31 @@ class MainAc : BaseBusinessAc<CountViewModel>() {
 
 
     override fun onViewCreated(decorView: View) {
-        Log.d(HiyHelper.tag, "onViewCreated")
-        viewModel.loadData()
-        val bookList = ServiceLoader.load(IProvider::class.java, javaClass.classLoader).toList()
-        bookList.forEach {
-            Log.d("MainActivity", it.name())
-        }
+        mRv = decorView.findViewById<RecyclerView>(R.id.rv)
+        mRv.layoutManager = GridLayoutManager(decorView.context, 4).apply {
+            this.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    val item = mData[position]
+                    return if (item is GridBo) 1 else 4
+                }
 
-
-
-        findViewById<View>(R.id.wsc).setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-
-            intent.data = Uri.parse("wsc://webview?url=${Uri.encode("www.baidu.com")}")
-            intent.resolveActivity(this@MainAc.packageManager)?.let {
-                this@MainAc.startActivity(intent)
             }
         }
+        mData.add(GridBo("1"))
+        mData.add(GridBo("2"))
+        mData.add(GridBo("3"))
+        mData.add(GridBo("4"))
+        val adapter = MultiTypeAdapter(mData).apply {
+            register(GridBo::class.java, GridItemViewBinder())
+        }
+        mRv.adapter = adapter
 
-        val fg = LifeCycleFg()
-        supportFragmentManager.beginTransaction().add(R.id.fg_container, fg).commit()
+        ItemClickSupport.addTo(mRv).setOnItemClickListener { recyclerView, position, v ->
+            Log.d(SodaConstant.TAG, "item click $position")
+        }
+
+
+        viewModel.loadData()
     }
 
     override fun onResume() {
